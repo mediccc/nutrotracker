8fc445ce05e36df606412298efe1d45eefbfa1b2
Yaroslav Chernous <yaroschernous@mail.ru>
2024-06-08 17:24:02 +0300

    #home page edited

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -110,71 +110,6 @@ export function HomePage() {
         {!accountIsAuth && (
           <div>Не авторизован.</div>
         )}
-        {accountIsAuth && (
-          <>
-            <div className='flex flex-row w-auto rounded-[16px] p-[16px] gap-[24px] bg-palette-dark-4'>
-            <div>Авторизован.</div>
-              <div className='flex w-auto'>{`EMAIL: ${accountUser.email}`}</div>
-              <div className='flex w-auto'>{`ID: ${accountUser.id}`}</div>
-              <Button size='small' type='submit' width='tight' priority='secondary' 
-                onClick={() => logoutAccount()}>
-                  Выйти из аккаунта
-              </Button>
-            </div>
-            <div className='flex flex-row gap-[24px]'>
-            <Card flex='col'>
-              <Text>Создание UFI</Text>
-              <FormProvider {...methodsCreateUfi}>
-                <form onSubmit={methodsCreateUfi.handleSubmit(submitCreateUfi)}>
-                  <Controller name='isFree' control={methodsCreateUfi.control} render={({ field: { value }, fieldState: { error }}) => 
-                      <InputFull name='Свободный' type='text' width='stretched' placeholder='Свободный' error={error} value={value} registerAs='isFree'
-                        onFocus={() => console.log('true')}
-                      />
-                    }
-                  />
-                  <Controller name='title' control={methodsCreateUfi.control} render={({ field: { value }, fieldState: { error }}) => 
-                      <InputFull name='Название' type='text' width='stretched' placeholder='Название' error={error} value={value} registerAs='title'
-                        onFocus={() => console.log('true')}
-                      />
-                    }
-                  />
-                  <Controller name='type' control={methodsCreateUfi.control} render={({ field: { value }, fieldState: { error }}) => 
-                      <InputFull name='Тип' type='text' width='stretched' placeholder='Свободный' error={error} value={value} registerAs='type'
-                        onFocus={() => console.log('true')}
-                      />
-                    }
-                  />
-                  <Button size='small' type='submit' priority='primary'>
-                    Создать UFI
-                  </Button>
-                </form>
-              </FormProvider>
-            </Card>
-            <Card flex='col'>
-              <Text>Создание MealDay</Text>
-              <FormProvider {...methodsCreateMealDay}>
-                <form onSubmit={methodsCreateMealDay.handleSubmit(submitCreateMealDay)}>
-                  <Controller name='isCompleted' control={methodsCreateMealDay.control} render={({ field: { value }, fieldState: { error }}) => 
-                      <InputFull name='Выполненность' type='text' width='stretched' placeholder='Выполненность' error={error} value={value} registerAs='isCompleted'
-                        onFocus={() => console.log('true')}
-                      />
-                    }
-                  />
-                  <Controller name='date' control={methodsCreateMealDay.control} render={({ field: { value }, fieldState: { error }}) => 
-                      <InputFull name='Дата' type='text' width='stretched' placeholder='Дата' error={error} value={value} registerAs='date'
-                        onFocus={() => console.log('true')}
-                      />
-                    }
-                  />
-                  <Button size='small' type='submit' priority='primary'>
-                    Создать MealDay
-                  </Button>
-                </form>
-              </FormProvider>
-            </Card>
-            </div>
-          </>
-        )}
         {!accountIsAuth && (
           <Card flex='col'>
           <Text>Регистрация</Text>
@@ -194,14 +129,8 @@ export function HomePage() {
                   field: { value }, 
                   fieldState: { error }
                 }) => 
-                  <Input
-                    name='Password'
-                    type='text'
-                    width='stretched'
-                    placeholder='password'
-                    error={error}
-                    value={value}
-                    registerAs='password'
+                  <InputFull name='Password' type='text' width='stretched' placeholder='password' error={error} value={value} registerAs='password'
+                    onFocus={() => console.log('true')}
                   />
                 }
               />
@@ -221,14 +150,8 @@ export function HomePage() {
                   field: { value }, 
                   fieldState: { error }
                 }) => 
-                  <Input
-                    name='Email'
-                    type='text'
-                    width='stretched'
-                    placeholder='email'
-                    error={error}
-                    value={value}
-                    registerAs='email'
+                  <InputFull name='Email' type='text' width='stretched' placeholder='email' error={error} value={value} registerAs='email'
+                    onFocus={() => console.log('true')}
                   />
                 }
               />
@@ -239,14 +162,8 @@ export function HomePage() {
                   field: { value }, 
                   fieldState: { error }
                 }) => 
-                  <Input
-                    name='Password'
-                    type='text'
-                    width='stretched'
-                    placeholder='password'
-                    error={error}
-                    value={value}
-                    registerAs='password'
+                  <InputFull name='Password' type='text' width='stretched' placeholder='password' error={error} value={value} registerAs='password'
+                    onFocus={() => console.log('true')}
                   />
                 }
               />
